1d747be89fc0759a8de3a1855d0c1df5defe2c8c
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-10 08:42:38 -0800

    Time: 38 ms (63.30%), Space: 13.8 MB (95.41%) - LeetHub

--- 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.py
+++ 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.py
@@ -12,3 +12,13 @@ class Solution:
                 
             return res
         
+        
+# Let a be the first number, then we will solve for the equation:
+#     a + a+1 + a+2 = num
+#     3a + 3 = num
+#     a = (num - 3) / 3
+    
+# Then if the first number is not an integer, we return empty list.
+# Else we return a list containing the first number and two consecutive
+# integers after it
+        
