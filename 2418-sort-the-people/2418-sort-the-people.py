b8b91918ea9111f7c5cb13cfbb68a0868924f822
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-17 07:30:06 -0800

    Time: 1711 ms (5.03%), Space: 14.5 MB (8.95%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(names)
+        m = n
         
         for passes in range(n):
             swap = False
-            for index in range(n-1):
+            for index in range(m-1):
                 if heights[index] < heights[index+1]:
                     swap = True
                     heights[index], heights[index+1] = heights[index+1], heights[index]
                     names[index], names[index+1] = names[index+1], names[index]
+            m -= 1
             
             if not swap:
                 break
