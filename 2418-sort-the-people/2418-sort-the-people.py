d7c764775fbe0f9a3f14b2f64834be86ed3b21f8
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-17 07:18:27 -0800

    Time: 2314 ms (5.03%), Space: 14.5 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -3,10 +3,15 @@ class Solution:
         n = len(names)
         
         for passes in range(n):
+            swap = False
             for index in range(n-1):
                 if heights[index] < heights[index+1]:
+                    swap = True
                     heights[index], heights[index+1] = heights[index+1], heights[index]
                     names[index], names[index+1] = names[index+1], names[index]
+            
+            if not swap:
+                break
                     
         return names
         
