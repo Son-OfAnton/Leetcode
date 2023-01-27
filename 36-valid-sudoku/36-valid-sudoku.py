9fc080c34965382bb948189b0f68027cd2d36ed6
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-27 02:44:59 -0800

    Time: 298 ms (5.07%), Space: 14 MB (29.17%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,4 +1,32 @@
 class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rx_elements = defaultdict(set)
+        cx_elements = defaultdict(set)
+        sub_grid = defaultdict(set)
+        
+        for rx in range(9):
+            for cx in range(9):
+                cell = board[rx][cx]
+                top_left_coordinate = (rx//3, cx//3)
+                
+                if cell != '.':
+                    if cell not in rx_elements[rx] and cell not in cx_elements[cx] \
+                    and cell not in sub_grid[top_left_coordinate]:
+                        rx_elements[rx].add(cell)
+                        cx_elements[cx].add(cell)
+                        sub_grid[top_left_coordinate].add(cell)
+                    else:
+                        return False
+                    
+        return True
+        
+
+
+
+
+
+"""
+class Solution:
     def is_valid(self, arr):
         arr = [char for char in arr if char != '.']
 
@@ -30,4 +58,4 @@ class Solution:
 
         return True
 
-    
+    """
