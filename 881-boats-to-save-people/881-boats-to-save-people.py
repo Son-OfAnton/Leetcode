90e26030ad7615dea93d3a2743dca3b310063a7a
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-28 01:41:27 -0800

    Time: 485 ms (60.59%), Space: 20.9 MB (25.29%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -1,24 +1,23 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
-        size = len(people)
         left = 0
-        right = size - 1
+        right = len(people) - 1
         boats = 0
         
         while left <= right:
             if people[left] + people[right] <= limit:
-                boats += 1
                 left += 1
-                right -= 1
-            
-            else:
-                if people[left] > people[right]:
-                    left += 1  
-                else:
-                    right -= 1
                 
-                boats += 1
+            right -= 1
+            boats += 1
                 
         return boats
+    
+# First we sort the people in order to easily find the heavies and 
+# lightest person. If we can place the heaviest and the lightest 
+# people on the boat we will place both of them then incerement left 
+# ptr and decrement right ptr. Else we will give precedence to the 
+# heaviest person and only decrement right ptr.
                     
+        
