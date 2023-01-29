c7ecb4eadba1d5d9b7c14b5b9b2f4747e55f9922
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-29 01:37:21 -0800

    Time: 41 ms (59.29%), Space: 15.4 MB (91.62%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -20,9 +20,24 @@ class Solution:
             dummy = temp
             
         head.next = None
-        head = curr
                 
-        return head
+        return curr
+    
+# We place two ptrs curr and dummy on head. In case of 0 or 1 node
+# return the head. Else we shift the dummy to the 2nd node and start
+# iterating on the list. 
+
+# In each iteration we create a temp ptr which points to the next node 
+# to the dummy, this prevents losing the next node since we will disconnect 
+# dummy to its next node. Then we disconnect dummy to its next node and 
+# connects it to curr. Then shift curr to dummy and dummy to temp. Do this 
+# until dummy reaches None. 
+
+# After we got out of the loop curr is at the tail node we break the link 
+# between the head and the second node. Doing this prevents cycle when we 
+# traverse in reverse order.
+
+
             
         
             
