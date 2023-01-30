456aacb3d0a641f1a0eb67ab69a17ac838ab20e9
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-30 07:53:01 -0800

    Time: 71 ms (35.13%), Space: 13.8 MB (61.34%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -4,7 +4,7 @@ class Solution:
         n = len(nums)
         
         for i in range(n):
-            for j in range(n - 1):
+            for j in range(n - 1 - i):
                 if nums[j] + nums[j+1] <= nums[j+1] + nums[j]:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
                     
