5d69d66f2d867f0242fc9e8e706f76b2794ee076
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-02-06 11:39:24 -0800

    Time: 207 ms, Memory: 15.2 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        arr_size = len(arr)
+        
+        if arr_size < 3:
+            return False
+        
+        for left in range(arr_size - 1):            
+            if arr[left] >= arr[left + 1]:
+                break
+            
+            
+        for right in range(arr_size - 1, 0, -1):            
+            if arr[right] >= arr[right - 1]:
+                break
+                
+        if left != right:
+            return False
+        
+        return True
+        
