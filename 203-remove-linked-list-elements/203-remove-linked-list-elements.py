2fd7a0e2966599513bf5054c7ca0268d6e25073d
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-02-10 03:15:44 -0800

    Time: 71 ms (69.57%), Space: 27 MB (5.03%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.py
@@ -3,27 +3,38 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+#         left = None
+#         right = head
+        
+#         while right:
+#             if right.val == val:
+#                 if right == head:
+#                     head = head.next
+#                     left = right
+#                     right = right.next
+#                 else:
+#                     while right and right.val == val:
+#                         right = right.next
+#                     left.next = right
+
+#             else:   
+#                 left = right
+#                 right = right.next
+            
+#         return head
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        left = None
-        right = head
-        
-        while right:
-            if right.val == val:
-                if right == head:
-                    head = head.next
-                    left = right
-                    right = right.next
-                else:
-                    while right and right.val == val:
-                        right = right.next
-                    left.next = right
-
-            else:   
-                left = right
-                right = right.next
+        if not head:
+            return head
+        else:
+            head.next = self.removeElements(head.next, val)
             
-        return head
+            if head.val == val:
+                return head.next
+            else:
+                return head
     
     
 
@@ -39,5 +50,5 @@ class Solution:
             if head.val == val:
                 return head.next
             else:
-                head
+                return head
 """
