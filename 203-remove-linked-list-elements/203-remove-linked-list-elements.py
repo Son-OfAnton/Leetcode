57cd47b1fd4bbbee8f2e168e20e61f0a37ccabfb
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-02-10 03:16:05 -0800

    Time: 69 ms (77.06%), Space: 17.8 MB (77.06%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.py
@@ -3,38 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# class Solution:
-#     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-#         left = None
-#         right = head
-        
-#         while right:
-#             if right.val == val:
-#                 if right == head:
-#                     head = head.next
-#                     left = right
-#                     right = right.next
-#                 else:
-#                     while right and right.val == val:
-#                         right = right.next
-#                     left.next = right
-
-#             else:   
-#                 left = right
-#                 right = right.next
-            
-#         return head
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        if not head:
-            return head
-        else:
-            head.next = self.removeElements(head.next, val)
+        left = None
+        right = head
+        
+        while right:
+            if right.val == val:
+                if right == head:
+                    head = head.next
+                    left = right
+                    right = right.next
+                else:
+                    while right and right.val == val:
+                        right = right.next
+                    left.next = right
+
+            else:   
+                left = right
+                right = right.next
             
-            if head.val == val:
-                return head.next
-            else:
-                return head
+        return head
     
     
 
