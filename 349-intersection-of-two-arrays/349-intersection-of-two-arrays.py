f602e72d77eaf8ccaf8d5791350faf1089aa4753
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-02-11 07:55:07 -0800

    Time: 52 ms (58.32%), Space: 14.1 MB (19.35%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,19 +1,28 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort()
-        nums2.sort()
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        
+        return list(nums1.intersection(nums2))
+    
+"""
+Two poitners approach
 
-        ptr_1, ptr_2 = 0, 0
-        res = set()
+nums1.sort()
+nums2.sort()
 
-        while ptr_1 < len(nums1) and ptr_2 < len(nums2):
-            if nums1[ptr_1] == nums2[ptr_2]:
-                res.add(nums1[ptr_1])
-                ptr_1 += 1
-                ptr_2 += 1
-            elif nums1[ptr_1] < nums2[ptr_2]:
-                ptr_1 += 1
-            else:
-                ptr_2 += 1
+ptr_1, ptr_2 = 0, 0
+res = set()
 
-        return list(res)
+while ptr_1 < len(nums1) and ptr_2 < len(nums2):
+    if nums1[ptr_1] == nums2[ptr_2]:
+        res.add(nums1[ptr_1])
+        ptr_1 += 1
+        ptr_2 += 1
+    elif nums1[ptr_1] < nums2[ptr_2]:
+        ptr_1 += 1
+    else:
+        ptr_2 += 1
+
+return list(res)
+"""
