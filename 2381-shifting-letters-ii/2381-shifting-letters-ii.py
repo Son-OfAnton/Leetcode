f5b38da863e2cd6fe2ebd9477edd340a40dc9a04
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-02-23 15:18:21 +0300

    Time: 1381 ms (62.57%), Space: 39.1 MB (82.12%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -14,17 +14,22 @@ class Solution:
                 
                 if shift[1] + 1 != size:
                     arr[shift[1] + 1] -= 1
-                
-                
-                
+        
         for i in range(1, len(arr)):
             arr[i] += arr[i - 1]
                 
         s = list(s)
-        print(arr)
         
         for i in range(len(arr)):
             s[i] = chr((((ord(s[i]) + arr[i]) - 97) % 26) + 97)
             
         return "".join(s)
             
+        
+# First we need a list of size len(s) which records the shift for each letter.
+# Then for every shift query's first index we will record the true shift(-1 for 0 and +1 for 1).
+# For the second index we record the opposite shift (+1 for 0 and -1 for 1). 
+# Then we will apply prefic sum on the record list. This creates an list which 
+# applies true shift for the wanted range and cancels the changes made on chars 
+# outside the range.
+# Finally we will convert the string into char list and update each of them.
