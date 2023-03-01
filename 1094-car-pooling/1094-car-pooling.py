08c0d5e9fab42780afd2ec19f917411f83965b33
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-01 18:21:51 +0300

    Time: 74 ms (72.00%), Space: 14.4 MB (80.57%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -3,14 +3,18 @@ class Solution:
         farthest = 0
         
         for pass_num, start, end  in trips:
-            farthest = max(farthest, end) + 1
+            farthest = max(farthest, end) + 1 # added 1 b/c it is 0 indexed
             
-        arr = [0] * (farthest + 1)
+        arr = [0] * (farthest + 1) # added 1 b/c i want the first to be 0
+                                   # help me in future comparision
         
         for pass_num, start, end in trips:
             arr[start + 1] += pass_num
-            arr[end + 1] -= pass_num
-            
+            arr[end + 1] -= pass_num    # deducting pass_num makes the element 
+                                        # -pass_num and doing prefix sum later
+                                        # only accumulates upto end.
+        
+        # doing prefix sum to load and unload passengers
         for index in range(1, farthest):
             arr[index] += arr[index - 1]
             
@@ -18,5 +22,6 @@ class Solution:
                 return False
             
         return True
+    
             
         
