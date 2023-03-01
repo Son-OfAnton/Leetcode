a96022a40f42e4472e57a97d175ecfb4e653ac51
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-01 13:49:52 +0300

    Time: 926 ms (84.12%), Space: 36.2 MB (52.50%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -13,4 +13,14 @@ class Solution:
         return len(stack)
     
 # We count fleets when slowest cars catch up the fast ones or when one
-# car goes by itself. To solve this we need to
+# car goes by itself. To solve this we need to calculate the time to 
+# reach the target to a car and append to a stack if stack is 
+# empty(since a one car is also a fleet) or if the time is larget than
+# the fleet infront of the car, this helps to maintain the increasing
+# monotonocity of the stack. The reason we append a car with larger time
+# ever seen is that it is guarenteed that it does reach the fleets infront
+# if it. Therefore it creates its own fleet and go by itself or maybe other 
+# cars from behind might join it. When they join it their common speed will be 
+# the new cars' this intern means the time at stack[-1] will be the largest.
+# Finally the stack contains times for each fleet and its length will be the 
+# the number of fleets.
