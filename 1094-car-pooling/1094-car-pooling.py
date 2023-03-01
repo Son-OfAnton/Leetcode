733dbc173e0723fbb6464140f51e83d64ab0a325
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-01 18:15:06 +0300

    Time: 77 ms (57.43%), Space: 14.4 MB (34.64%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -2,14 +2,14 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         farthest = 0
         
-        for trip in trips:
-            farthest = max(farthest, trip[2]) + 1
+        for pass_num, start, end  in trips:
+            farthest = max(farthest, end) + 1
             
         arr = [0] * (farthest + 1)
         
-        for trip in trips:
-            arr[trip[1] + 1] += trip[0] 
-            arr[trip[2] + 1] -= trip[0]
+        for pass_num, start, end in trips:
+            arr[start + 1] += pass_num
+            arr[end + 1] -= pass_num
             
         for index in range(1, farthest):
             arr[index] += arr[index - 1]
