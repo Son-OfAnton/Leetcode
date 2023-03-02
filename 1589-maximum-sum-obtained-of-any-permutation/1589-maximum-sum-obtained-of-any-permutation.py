b22a2ca24503bd98b34a25dc43828207571a29fc
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-02 21:00:33 +0300

    Time: 1666 ms (69.44%), Space: 48.1 MB (55.56%) - LeetHub

---
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        n = len(nums)
+        pre = [0] * (n + 1)     # multiplying by n+1 to avoid if case 
+                                # later in decrementing 1 from end+1
+        
+        for start, end in requests:
+            pre[start] += 1
+            pre[end + 1] -= 1       # multiplying by n+1 helps to avoid 
+                                    # if case here
+            
+        for i in range(1, n + 1):
+            pre[i] += pre[i - 1]
+
+        pre.pop()       # remove that space we added to avoid if case
+        nums.sort()
+        pre.sort()
+        max_sum = 0
+        
+        for index in range(n):
+            max_sum += nums[index] * pre[index]
+            
+        return max_sum % (10**9 + 7)
