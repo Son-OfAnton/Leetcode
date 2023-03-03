48c97aeb9183e494fcd708eab92d127b0d613aff
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-03 23:16:04 +0300

    Time: 31 ms (75.54%), Space: 13.8 MB (95.43%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -2,19 +2,19 @@ class Solution:
     
     
     def __init__(self):
-        self.store = [[1], [1,1]]        
+        self.rows = [[1], [1,1]]        
         
         
     def getRow(self, rowIndex: int) -> List[int]:
-        for _ in range(rowIndex - len(self.store) + 1):
+        for _ in range(rowIndex - len(self.rows) + 1):
             L, R = 0, 1
             new_row = [1]
 
-            while R < len(self.store[-1]):
-                new_row.append(self.store[-1][L] + self.store[-1][R])
+            while R < len(self.rows[-1]):
+                new_row.append(self.rows[-1][L] + self.rows[-1][R])
                 L += 1
                 R += 1
             new_row.append(1)
-            self.store.append(new_row)
+            self.rows.append(new_row)
             
-        return self.store[rowIndex]
+        return self.rows[rowIndex]
