7c27fe0ae0780b33eceba25c533827d012d4aab5
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-03 23:32:24 +0300

    Time: 30 ms (80.34%), Space: 13.8 MB (95.43%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -1,20 +1,53 @@
 class Solution:
+    """
+    Recursive solution
+    """
     
-    
-    def __init__(self):
-        self.rows = [[1], [1,1]]        
-        
-        
     def getRow(self, rowIndex: int) -> List[int]:
-        for _ in range(rowIndex - len(self.rows) + 1):
-            L, R = 0, 1
-            new_row = [1]
-
-            while R < len(self.rows[-1]):
-                new_row.append(self.rows[-1][L] + self.rows[-1][R])
-                L += 1
-                R += 1
-            new_row.append(1)
-            self.rows.append(new_row)
+        if rowIndex == 0:
+            return [1]
+        
+        upper_row = self.getRow(rowIndex - 1)
+        curr_row = [1]
+        
+        for index in range(rowIndex - 1):
+            curr_row.append(upper_row[index] + upper_row[index + 1])
+        
+        curr_row.append(1)
+        
+        return curr_row
             
-        return self.rows[rowIndex]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+"""
+Iterative DP solution
+
+def __init__(self):
+    self.rows = [[1], [1,1]]        
+
+
+def getRow(self, rowIndex: int) -> List[int]:
+    for _ in range(rowIndex - len(self.rows) + 1):
+        L, R = 0, 1
+        new_row = [1]
+
+        while R < len(self.rows[-1]):
+            new_row.append(self.rows[-1][L] + self.rows[-1][R])
+            L += 1
+            R += 1
+        new_row.append(1)
+        self.rows.append(new_row)
+
+    return self.rows[rowIndex]
+"""
