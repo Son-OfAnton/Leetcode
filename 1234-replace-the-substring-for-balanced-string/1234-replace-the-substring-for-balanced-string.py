eda8904c216745a7fc29a4423d70c9e8df7a8ed7
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-05 00:37:23 +0300

    Time: 223 ms (75.41%), Space: 14.7 MB (77.05%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -3,30 +3,18 @@ class Solution:
         n = len(s)
         normal_count = n / 4
         count = Counter(s)
-#         too_much = dict()
-        
-#         for char in s:
-#             if count[char] > normal_count:
-#                 too_much[char] = count[char] - normal_count
-        
         too_much = {c: max(0, count[c] - normal_count) for c in count}
-        
-        # if not too_much:
-        #     return 0
                 
         min_substring = n
         left = 0
         
         for right, char in enumerate(s):
-            # if char in too_much:
             too_much[char] -= 1
             
             while max(too_much.values()) <= 0 and left < n:
                 min_substring = min(min_substring, right - left + 1)
                 
-                # if s[left] in too_much:
-                too_much[s[left]] += 1
-                    
+                too_much[s[left]] += 1                    
                 left += 1
                 
         return min_substring 
