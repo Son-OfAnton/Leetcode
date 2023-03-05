8824c220f091f3a2745d049eafae865d06f55227
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-05 22:45:54 +0300

    Time: 43 ms (38.72%), Space: 14.1 MB (22.84%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,29 +5,61 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        curr_1, curr_2 = list1, list2
-        merged_head = ListNode()
-        curr_merged = merged_head
+        """
+        Recursive approach
+        """
+        if not list1:
+            return list2
+        if not list2:
+            return list1
         
-        while curr_1 and curr_2:
-            if curr_1.val <= curr_2.val:
-                curr_merged.next = curr_1
-                curr_1 = curr_1.next
-            else:
-                curr_merged.next = curr_2
-                curr_2 = curr_2.next
-                
-            curr_merged = curr_merged.next
-            
-        while curr_1:
-            curr_merged.next = curr_1
-            curr_merged = curr_merged.next
-            curr_1 = curr_1.next
-            
-        while curr_2:
-            curr_merged.next = curr_2
-            curr_merged = curr_merged.next
-            curr_2 = curr_2.next
+        merged = ListNode()
         
-        return merged_head.next
-                
+        if list1.val > list2.val:
+            merged.val = list2.val
+            merged.next = self.mergeTwoLists(list1, list2.next)
+        else:
+            merged.val = list1.val
+            merged.next = self.mergeTwoLists(list1.next, list2)
+            
+        return merged
+        
+        
+        
+        
+        
+        
+        
+"""
+Iterative approach
+
+
+curr_1, curr_2 = list1, list2
+merged_head = ListNode()
+curr_merged = merged_head
+
+while curr_1 and curr_2:
+    if curr_1.val <= curr_2.val:
+        curr_merged.next = curr_1
+        curr_1 = curr_1.next
+    else:
+        curr_merged.next = curr_2
+        curr_2 = curr_2.next
+
+    curr_merged = curr_merged.next
+
+while curr_1:
+    curr_merged.next = curr_1
+    curr_merged = curr_merged.next
+    curr_1 = curr_1.next
+
+while curr_2:
+    curr_merged.next = curr_2
+    curr_merged = curr_merged.next
+    curr_2 = curr_2.next
+
+return merged_head.next
+
+"""
+
+
