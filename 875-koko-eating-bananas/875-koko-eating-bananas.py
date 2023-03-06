46cf69f5ec2fef1a6dc7350ef090d25322cae3a2
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-06 15:34:08 +0300

    Time: 449 ms (83.75%), Space: 15.4 MB (63.44%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -2,6 +2,7 @@ class Solution:
     def calculateTime(self, piles: List[int], speed: int) -> int:
         if speed == 0:
             return float('inf')
+        
         time = 0
         
         for pile in piles:
