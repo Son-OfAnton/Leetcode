5d12c3c325b7c38826c2d290c488edf72b55a601
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-09 00:16:22 +0300

    Time: 142 ms (76.15%), Space: 20.7 MB (21.33%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.py
+++ 275-h-index-ii/275-h-index-ii.py
@@ -1,11 +1,8 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        if set(citations) == {0}:
-            return 0
         n = len(citations)
-        
-        res = 0
         left, right = 0, n - 1
+        h_index = 0
         
         while left <= right:
             mid = left + (right - left) // 2
@@ -13,9 +10,7 @@ class Solution:
             if n - mid > citations[mid]:
                 left = mid + 1
             elif n - mid <= citations[mid]:
-                res = max(n - mid, res)
+                h_index = max(n - mid, h_index)
                 right = mid - 1
-            else:
-                return citations[mid]
-            
-        return res
+                
+        return h_index
