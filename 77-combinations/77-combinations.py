73290aacf5ad533dd2490d3c6ba1149aeb704900
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-09 11:20:55 +0300

    Time: 449 ms (35.54%), Space: 15.9 MB (83.04%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def __init__(self):
+        self.solutions = []
+        
+    
+    def backtrack(self, path, start, level, n):
+        if level == 0:
+            self.solutions.append(path)
+            return 
+        
+        for num in range(start, n + 1):
+            self.backtrack(path + [num], num + 1, level - 1, n)
+            
+            
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.backtrack([], 1, k, n)
+        
+        return self.solutions
