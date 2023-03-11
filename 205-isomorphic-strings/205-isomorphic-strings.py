2b2994735bf98225a577d8ff06c55aecaeadd5bc
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-11 23:32:08 +0300

    Time: 42 ms (64.89%), Space: 14.1 MB (81.49%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        char_map = {}
+        
+        for i in range(len(s)):
+            if s[i] in char_map:
+                if t[i] != char_map[s[i]]:
+                    return False
+            else:
+                if t[i] in char_map.values():
+                    return False
+                
+                char_map[s[i]] = t[i]
+        
+        return True
