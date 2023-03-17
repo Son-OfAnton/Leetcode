fff57800b55020b242bb4b588c336ac15723dcd9
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-17 22:45:24 +0300

    Time: 27 ms (88.20%), Space: 13.9 MB (59.31%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,19 +1,60 @@
 class Solution:
-    def decodeString(self, s: str) -> str:
-        stack = []
+    def __init__(self):
+        self.s = None
+        
+    def decoder(self, index):
+        """
+        Recursive solution
+        """
+        decoded = ""
+        mult = 0
 
-        for char in s:
-            if char != ']':
-                stack.append(char)
+        while index < len(self.s):
+            char = self.s[index]
+
+            if char.isdigit():
+                mult = mult * 10 + int(char)
+            elif char == '[':
+                inner_string, last_index = self.decoder(index + 1)
+                decoded += mult * inner_string
+                index = last_index
+                mult = 0
+            elif char == ']':
+                return decoded, index
             else:
-                inner = ""
-                while stack[-1] != '[':
-                    inner = stack.pop() + inner
-                stack.pop()
+                decoded += char
+            
+            index += 1
 
-                num = ""
-                while stack and stack[-1].isdigit():
-                    num = stack.pop() + num
-                stack.append(int(num) * inner)
+        return decoded, index
 
-        return "".join(stack)
+    def decodeString(self, s: str) -> str:
+        self.s = s
+        
+        return self.decoder(0)[0]
+
+    
+    
+    
+"""
+Iterative solution
+
+stack = []
+
+for char in s:
+    if char != ']':
+        stack.append(char)
+    else:
+        inner = ""
+        while stack[-1] != '[':
+            inner = stack.pop() + inner
+        stack.pop()
+
+        num = ""
+        while stack and stack[-1].isdigit():
+            num = stack.pop() + num
+        stack.append(int(num) * inner)
+
+return "".join(stack)
+
+"""
