8e85d7d82fcca83443d6a2fa6d56464226c3efe5
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-18 14:15:18 +0300

    Time: 51 ms (40.25%), Space: 18.9 MB (13.36%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -8,22 +8,35 @@ class Solution:
     def __init__(self):
         self.level_index_map = defaultdict(list)
 
-    def dfs(self, node, level, column):
+    def dfs(self, node, level, index):
         if not node:
             return
 
-        self.level_index_map[level].append(column)
-        self.dfs(node.left, level + 1, column * 2)
-        self.dfs(node.right, level + 1, column * 2 + 1)
+        self.level_index_map[level].append(index)
+        self.dfs(node.left, level + 1, index * 2)
+        self.dfs(node.right, level + 1, index * 2 + 1)
     
     def widthOfBinaryTree(self, root: TreeNode) -> int:
         self.dfs(root, 0, 0)
         max_width = -1
 
         for level in self.level_index_map:
-            curr_indexes = self.level_index_map[level]
-            max_width = max(max_width, max(curr_indexes) - min(curr_indexes) + 1)
+            curr_level_indexes = self.level_index_map[level]
+            max_width = max(max_width, max(curr_level_indexes) - min(curr_level_indexes) + 1)
 
         return max_width
-        
 
+    
+    
+"""    
+ 
+                             0
+                    0                 1 
+               0        1        2          3
+           0       1   2  3    4    5     6    7
+           
+
+We can visualize the tree as above where the left child's index is 2 * index_of_parent and 
+right child's index is 2 * index_of_parent + 1.
+
+"""
