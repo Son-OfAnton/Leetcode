4bd4df9bffe69a7dd613aff0cc70790448e3a139
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-20 23:04:16 +0300

    Time: 2397 ms (5.08%), Space: 14 MB (38.64%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,8 +1,7 @@
 class Solution:
     def __init__(self):
         self.max_len = 0
-        self.arr = None
-        self.n = None
+        
         
     def checkUniqueness(self, sub_seq):
         string = "".join(sub_seq)
@@ -10,20 +9,21 @@ class Solution:
         if len(string) == len(set(string)):
             self.max_len = max(self.max_len, len(string))
         
-    def backtrack(self, i, sub_seq):
-        if i == self.n:
-            self.checkUniqueness(sub_seq)
-            return
-
-        sub_seq.append(self.arr[i])
-        self.checkUniqueness(sub_seq)        
-        self.backtrack(i + 1, sub_seq)
-        sub_seq.pop()
-        self.backtrack(i + 1, sub_seq)
         
     def maxLength(self, arr: List[str]) -> int:
-        self.arr = arr
-        self.n = len(arr)            
-        self.backtrack(0, [])
+        n = len(arr)
+        
+        def backtrack(i, sub_seq):
+            if i == n:
+                self.checkUniqueness(sub_seq)
+                return
+
+            sub_seq.append(arr[i])
+            self.checkUniqueness(sub_seq)        
+            backtrack(i + 1, sub_seq)
+            sub_seq.pop()
+            backtrack(i + 1, sub_seq)
+            
+        backtrack(0, [])
         
         return self.max_len
