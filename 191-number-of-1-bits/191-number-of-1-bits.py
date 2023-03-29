cf75495f0d352a055b5b43b64148ae3dbb7ed093
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-29 22:12:22 +0300

    Time: 31 ms (73.47%), Space: 13.8 MB (38.75%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,5 +1,8 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        """
+        Time O(m) m - the number of 1 bits
+        """
         count = 0
         
         while n:
@@ -7,5 +10,17 @@ class Solution:
             count += 1
             
         return count
-            
-            
+    
+    
+"""
+mask = 1
+    one_count = 0
+
+    for i in range(32):
+        if n & mask:
+            one_count += 1
+        mask <<= 1
+
+    return one_count
+"""
+
