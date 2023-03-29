59a404bd0fb178c567a96b1582d5c42a0e7b00cc
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-29 21:37:58 +0300

    Time: 141 ms (49.10%), Space: 15.1 MB (66.25%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,22 +1,42 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
+        XOR_of_nums = 0
+        XOR_of_range = 0
+        
+        for i, num in enumerate(nums):
+            XOR_of_nums ^= num
+            XOR_of_range ^= i
+            
+        XOR_of_range ^= len(nums)
+        
+        return XOR_of_nums ^ XOR_of_range
+            
+        
+        
+"""
+cyclic sort
 
-        for curr in range(n):
-            pos = nums[curr]
-            
-            while pos < n and pos != curr:
-                nums[pos], nums[curr] = nums[curr], nums[pos]
-                pos = nums[curr]
-                
-        for i in range(n):
-            if i != nums[i]:
-                return i
-            
-        return n
+n = len(nums)
+
+for curr in range(n):
+    pos = nums[curr]
+
+    while pos < n and pos != curr:
+        nums[pos], nums[curr] = nums[curr], nums[pos]
+        pos = nums[curr]
+
+for i in range(n):
+    if i != nums[i]:
+        return i
+
+return n
+
+"""
     
 
 """
+math solution
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
