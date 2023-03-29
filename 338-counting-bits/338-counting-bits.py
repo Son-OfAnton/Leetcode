b4d5937f2f11acb7eafeeee4bc6bcaa30f31ea0a
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-29 21:52:23 +0300

    Time: 86 ms (66.03%), Space: 20.8 MB (19.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,19 +1,35 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        """Time O(n)"""
+        
         ans = [0] * (n + 1)
         mask = 1
         
-        for i in range(n+1):
-            num = i
-            
-            count = 0 
-            while num > 0:
-                if num & mask == 1:
-                    count += 1
-                num >>= 1
-            ans[i] = count
-            
+        for num in range(n + 1):
+            if num & mask == 1:    # if num is odd
+                ans[num] = 1 + ans[num >> 1]
+            else:
+                ans[num] = ans[num >> 1]
+                
         return ans
-                    
-            
         
+        
+"""
+Time O(nlogn)
+
+ans = [0] * (n + 1)
+mask = 1
+
+for i in range(n+1):
+    num = i
+
+    count = 0 
+    while num > 0:
+        if num & mask == 1:
+            count += 1
+        num >>= 1
+    ans[i] = count
+
+return ans
+
+    """
