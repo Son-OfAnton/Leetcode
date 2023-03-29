ba0c2d45ddf663abb74f5b40968a1ff73e54241b
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-30 00:36:44 +0300

    Time: 43 ms (51.76%), Space: 14.1 MB (16.83%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,19 +1,45 @@
 class Solution:
-    def backtrack(self, i, nums):
-        if i == len(nums) - 1:
-            return [[nums[i]]]  # last element's permutation is itself
-        
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         permutations = []
-        curr_permutaions = self.backtrack(i + 1, nums)
+        seen = set()
         
-        for p in curr_permutaions:
-            for index in range(len(p) + 1):
-                p_copy = p.copy()
-                p_copy.insert(index, nums[i])
-                permutations.append(p_copy)
+        def backtrack(i, candidate):
+            if i == n:
+                permutations.append(candidate.copy())
                 
+            for index in range(n):
+                if nums[index] not in seen:
+                    candidate.append(nums[index])
+                    seen.add(nums[index])
+                    backtrack(i + 1, candidate)
+                    seen.remove(nums[index])
+                    candidate.pop()
+                    
+        backtrack(0, [])
+        
         return permutations
     
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        return self.backtrack(0, nums)
-        
+
+"""
+Time O(n^2 * n!)
+Space O(n!)
+
+def backtrack(self, i, nums):
+    if i == len(nums) - 1:
+        return [[nums[i]]]  # last element's permutation is itself
+
+    permutations = []
+    curr_permutaions = self.backtrack(i + 1, nums)
+
+    for p in curr_permutaions:
+        for index in range(len(p) + 1):
+            p_copy = p.copy()
+            p_copy.insert(index, nums[i])
+            permutations.append(p_copy)
+
+    return permutations
+
+def permute(self, nums: List[int]) -> List[List[int]]:
+    return self.backtrack(0, nums)
+"""
