5d271260579036d12ecabce2d5b1dad3e3fbe8d7
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-30 23:22:54 +0300

    Time: 47 ms (24.47%), Space: 14.3 MB (16.77%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -6,7 +6,7 @@ class Solution:
         
         def backtrack(candidate):
             if len(candidate) == n:
-                permutations.append(candidate[:])
+                permutations.append(candidate.copy())
                 return
             
             for index in range(n):
