d4a82fd082f4e834eb80983c18dbaf602295778c
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-30 18:04:42 +0300

    Time: 105 ms (67.23%), Space: 18.8 MB (42.44%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,38 @@
+class Solution:
+    def quickSelect(self, num_freq, start, end, needed_index):
+        if start >= end:
+            return
+        
+        # pivot = num_freq[start]
+        write = start + 1
+
+        for read in range(start + 1, end + 1):
+            if num_freq[read][1] <= num_freq[start][1]:
+                num_freq[read], num_freq[write] = num_freq[write], num_freq[read]
+                write += 1
+
+        num_freq[start], num_freq[write - 1] = num_freq[write - 1], num_freq[start]
+
+        if write > needed_index:
+            self.quickSelect(num_freq, start, write - 2, needed_index)
+        else:
+            self.quickSelect(num_freq, write, end, needed_index)
+
+
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq_map = Counter(nums)
+        num_freq = [[num, freq] for num, freq in freq_map.items()]
+        # print(f"before sort {num_freq}")
+        n = len(num_freq)
+
+        self.quickSelect(num_freq, 0, n - 1, n - k)
+        # print(f"after sort {num_freq}")
+        res = []
+        
+        for index in range(n - k, n):
+            res.append(num_freq[index][0])
+            
+        return res
+
+
