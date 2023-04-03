66ce44b65061b10c8d0643972d765d7de0c1936b
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-04-03 15:41:22 +0300

    Time: 2857 ms (5.10%), Space: 14 MB (36.67%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.py
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -1,11 +1,10 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
-        permutations = []
-        bit_mask = 0
+        mask = 0
         beautiful_arrangements = 0
         
         def backtrack(index, candidate):
-            nonlocal bit_mask, beautiful_arrangements
+            nonlocal mask, beautiful_arrangements
 
             if index == n:
                 beautiful_arrangements += 1
@@ -14,14 +13,14 @@ class Solution:
             for i in range(1, n + 1):
                 shifted_num = 1 << i
                 
-                if bit_mask & shifted_num == 0:
+                if mask & shifted_num == 0:
                     candidate.append(i)
-                    bit_mask |= shifted_num
+                    mask |= shifted_num
 
                     if candidate[index] % (index + 1) == 0 or (index + 1) % candidate[index] == 0:
                         backtrack(index + 1, candidate)
                     
-                    bit_mask &= ~shifted_num
+                    mask &= ~shifted_num
                     candidate.pop()
                     
         backtrack(0, [])
