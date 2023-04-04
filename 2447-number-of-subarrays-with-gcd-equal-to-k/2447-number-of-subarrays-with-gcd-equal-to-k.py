0cd7373d0d3694e3a95c04ca73356c05af4787b2
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-04-04 21:42:45 +0300

    Time: 340 ms (39.66%), Space: 14 MB (71.26%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.py
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.py
@@ -13,7 +13,7 @@ class Solution:
             curr_GCD = 0
             
             j = i
-            while j < len(nums) and nums[j] % k == 0:
+            while j < n and nums[j] % k == 0:
                 curr_GCD = self.gcd(curr_GCD, nums[j])
                 
                 if curr_GCD == k:
