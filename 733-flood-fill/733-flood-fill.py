3055223ac64abcae8c63d21973b8e487bc2edf3d
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-04-08 02:10:35 +0300

    Time: 77 ms (67.16%), Space: 14.3 MB (10.67%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -6,25 +6,23 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         visited = set()
-        after_fill = copy.deepcopy(image)
-        after_fill[sr][sc] = color
         
         def dfs(row, col):
             if (row, col) in visited:
                 return
 
             visited.add((row, col))
-
+            prev_color = image[row][col]
+            image[row][col] = color
+            
             for row_change, col_change in directions:
                 new_row = row + row_change
                 new_col = col + col_change
 
                 if self.in_bound(new_row, new_col, len(image), len(image[0])) and (new_row, new_col) not in visited:
-                    if image[new_row][new_col] == image[row][col]:
-                        after_fill[new_row][new_col] = color
+                    if image[new_row][new_col] == prev_color:
                         dfs(new_row, new_col)
         
         dfs(sr, sc)
-    
         
-        return after_fill
+        return image
