b7076e678798ba150b3bba0be284c3074d50b426
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-04-12 23:19:04 +0300

    Time: 102 ms (62.37%), Space: 15.8 MB (16.57%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,10 +1,8 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         res_path = []
-        visited = set()
 
         def dfs(node: int, path: List[int]):
-            visited.add(node)
             path.append(node)
 
             if node == len(graph) - 1:
