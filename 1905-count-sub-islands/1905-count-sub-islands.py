d59a89fb7fe987bdb73fe47899b1aa94fa1c732b
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-04-13 22:32:10 +0300

    Time: 3702 ms (16.08%), Space: 97.1 MB (37.52%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,31 +1,38 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        rows, cols = len(grid2), len(grid2[0])
+        row, col = len(grid2), len(grid2[0])
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        visited = [[False] * cols for _ in range(rows)]
+        visited = set()
         
-        def dfs(row_index, col_index):
-            if not (0 <= row_index < rows and 0 <= col_index < cols) or grid2[row_index][col_index] == 0 or visited[row_index][col_index]:
+        def inbound(rx, cx):
+            return 0 <= rx < row and 0 <= cx < col
+        
+        def dfs(rx, cx):
+            if not inbound(rx, cx) or grid2[rx][cx] == 0 or (rx, cx) in visited:
                 return True
 
-            is_curr_island = True
-            if grid1[row_index][col_index] == 0:
-                is_curr_island = False
+            curr = True
+            if grid1[rx][cx] == 0:
+                curr = False
 
-            visited[row_index][col_index] = True
+            visited.add((rx, cx))
 
-            for direction in directions:
-                new_row_index, new_col_index = row_index + direction[0], col_index + direction[1]
-                is_curr_island &= dfs(new_row_index, new_col_index)
+            for _dir in directions:
+                new_rx, new_cx = rx + _dir[0], cx + _dir[1]
+                curr &= dfs(new_rx, new_cx)
 
-            return is_curr_island
+            return curr
             
         sub_island_count = 0
         
-        for row_index in range(rows):
-            for col_index in range(cols):
-                if grid2[row_index][col_index] == 1 and not visited[row_index][col_index]:
-                    if dfs(row_index, col_index):
+        for rx in range(row):
+            for cx in range(col):
+                if grid2[rx][cx] == 1 and (rx, cx) not in visited:
+                    if dfs(rx, cx):
                         sub_island_count += 1         
                     
         return sub_island_count
+                    
+            
+                
+            
