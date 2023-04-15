3f3a83dc26e6462f9d91938d9783764ccea9c414
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-04-15 23:17:13 +0300

    Time: 800 ms (61.11%), Space: 74.1 MB (24.60%) - LeetHub

---
+++ 1600-throne-inheritance/1600-throne-inheritance.py
@@ -0,0 +1,38 @@
+class ThroneInheritance:
+
+    def __init__(self, kingName: str):
+        self.kingName = kingName
+        self.family_tree = defaultdict(list)
+        self.family_tree[kingName]
+        self.cemetery = set()
+
+    def birth(self, parentName: str, childName: str) -> None:
+        self.family_tree[parentName].append(childName)                
+
+    def death(self, name: str) -> None:
+        self.cemetery.add(name)
+
+    def getInheritanceOrder(self) -> List[str]:
+        curr_order = []
+        visited = set()
+        
+        def dfs(person):
+            if person not in self.cemetery:
+                curr_order.append(person)
+            visited.add(person)
+            
+            for child in self.family_tree[person]:
+                if child not in visited:
+                    dfs(child)
+                    
+        dfs(self.kingName)
+                    
+        return curr_order
+        
+
+
+# Your ThroneInheritance object will be instantiated and called as such:
+# obj = ThroneInheritance(kingName)
+# obj.birth(parentName,childName)
+# obj.death(name)
+# param_3 = obj.getInheritanceOrder()
