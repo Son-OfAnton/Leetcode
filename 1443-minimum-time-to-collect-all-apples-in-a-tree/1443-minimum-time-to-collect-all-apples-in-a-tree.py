a7eba7c5c795c8c191e38d7125b51d890896aca0
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-04-29 22:09:34 +0300

    Time: 718 ms (32.44%), Space: 53.7 MB (43.20%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -6,20 +6,19 @@ class Solution:
             graph[u].append(v)
             graph[v].append(u)
 
-        def dfs(node, visited):
+        def dfs(node, parent):
             time = 0
-            visited.add(node)
 
             for child in graph[node]:
-                if child not in visited:
-                    subtree_time = dfs(child, visited)
+                if child != parent:
+                    subtree_time = dfs(child, node)
 
                     if subtree_time or hasApple[child]:
                         time += subtree_time + 2
 
             return time 
 
-        return dfs(0, set())
+        return dfs(0, None)
             
             
             
