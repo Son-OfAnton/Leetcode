f32c5c325259463d872a72023d6d8b719960c41d
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-01 16:51:19 +0300

    Time: 874 ms (19.59%), Space: 17 MB (7.17%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,27 @@
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        m, n = len(maze), len(maze[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        def on_border(rx, cx):
+            return rx == 0 or rx == m - 1 or cx == 0 or cx == n - 1
+        def inbound(rx, cx):
+            return 0 <= rx < m and 0 <= cx < n
+        
+        queue = deque([(entrance[0], entrance[1], 0)])
+        maze[entrance[0]][entrance[1]] = "+"
+
+        while queue:
+            row, col, steps = queue.popleft()
+                
+            for dr, dc in directions:
+                new_row, new_col = row + dr, col + dc
+
+                if inbound(new_row, new_col) and maze[new_row][new_col] == '.':
+                    if [new_row, new_col] != entrance and on_border(new_row, new_col):
+                        return steps + 1
+
+                    queue.append((new_row, new_col, steps + 1))
+                    maze[new_row][new_col] = "+"
+
+        return -1
