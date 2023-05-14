1dca27a014797d56d2ef5759aa5ce65fa086da54
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-15 01:41:01 +0300

    Time: 396 ms (64.52%), Space: 17.7 MB (59.24%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.py
+++ 934-shortest-bridge/934-shortest-bridge.py
@@ -1,20 +1,21 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         n = len(grid)
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
         def inbound(rx, cx):
             return 0 <= rx < n and 0 <= cx < n
-
+        
+        # finding the fist land
         island_1_land = None
-
         for i in range(n):
             for j in range(n):
                 if grid[i][j] == 1:
                     island_1_land = (i, j)
                     break
 
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
+        # after finding the first land all lands
+        # in its component are parts of island_1
         island_1_queue = deque([island_1_land])
         island_1 = set([island_1_land])
 
@@ -27,8 +28,11 @@ class Solution:
                 if inbound(nr, nc) and grid[nr][nc] == 1 and (nr, nc) not in island_1:
                     island_1_queue.append((nr, nc))
                     island_1.add((nr, nc))
+                    
 
-
+        # now doing multi-source bfs from 
+        # all lands in island_1 and return 
+        # the first land which is not in island_1
         queue = deque()
         for land in island_1:
             queue.append((land, 0))
