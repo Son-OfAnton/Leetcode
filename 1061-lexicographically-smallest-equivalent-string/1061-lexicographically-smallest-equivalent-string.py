8a413f98c216eb125bbe59b303251a805ea69721
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-18 00:20:25 +0300

    Time: 69 ms (10.42%), Space: 16.4 MB (27.08%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,9 +1,9 @@
-class Disjoint_set:
+class Union_find:
     def __init__(self, n):
         self.rep = [i for i in range(n)]
         self.size = [1 for _ in range(n)]
 
-    def representative(self, x: int) -> int:
+    def find(self, x: int) -> int:
         parent = x
 
         while parent != self.rep[parent]:
@@ -17,33 +17,30 @@ class Disjoint_set:
         return parent
 
     def union(self, x: int, y: int) -> None:
-        x_rep = self.representative(x)
-        y_rep = self.representative(y)
+        x_rep = self.find(x)
+        y_rep = self.find(y)
 
         if x_rep != y_rep:
             greater = x_rep if self.size[x_rep] >= self.size[y_rep] else y_rep
             smaller = y_rep if greater == x_rep else x_rep
             self.rep[smaller] = greater
             self.size[greater] +=  self.size[smaller]
-
-    def connected(self, x: int, y: int) -> bool:
-        return self.representative(x) == self.representative(y)
     
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        ds = Disjoint_set(26)
+        uf = Union_find(26)
         
         for i in range(len(s1)):
-            ds.union(ord(s1[i]) - 97, ord(s2[i]) - 97)
+            uf.union(ord(s1[i]) - 97, ord(s2[i]) - 97)
             
         groups = defaultdict(set)
         
         for i in range(26):
-            groups[(ds.rep[i])].add(chr(i + 97))
+            groups[(uf.rep[i])].add(chr(i + 97))
 
         res = []
         for char in baseStr:
-            rep = ds.representative(ord(char) - 97)
+            rep = uf.find(ord(char) - 97)
             res.append(min(groups[rep]))
 
         return ''.join(res)
