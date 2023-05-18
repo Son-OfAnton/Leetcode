e740f7eed194005c697048a18831f44b7b25b94f
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-18 19:20:15 +0300

    Time: 1987 ms (25.03%), Space: 61.1 MB (81.29%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,47 @@
+class Union_find:
+    def __init__(self, n):
+        self.rep = [[i, float('inf')] for i in range(n)]
+        self.size = [1 for _ in range(n)]
+
+    def find(self, x: int) -> int:
+        parent = x
+
+        while parent != self.rep[parent][0]:
+            parent = self.rep[parent][0]
+
+        while x != parent:
+            prev_parent = self.rep[x][0]
+            self.rep[x][0] = parent
+            x = prev_parent
+
+        return parent
+
+    def union(self, x: int, y: int, dist: int) -> None:
+        x_rep = self.find(x)
+        y_rep = self.find(y)
+
+        greater = x_rep if self.size[x_rep] >= self.size[y_rep] else y_rep
+        smaller = y_rep if greater == x_rep else x_rep
+        self.rep[smaller][0] = greater
+        self.size[greater] +=  self.size[smaller]
+        self.rep[greater][1] = min(self.rep[greater][1], self.rep[smaller][1], dist)
+            
+    def connected(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        uf = Union_find(n + 1)
+
+        for u, v, dist in roads:
+            uf.union(u, v, dist)
+
+        res = float('inf')
+        one_parent = uf.find(1)
+        for i in range(1, n + 1):
+            if one_parent == uf.find(i):
+                res = min(res, uf.rep[uf.find(i)][1])
+
+        return res 
+
+
