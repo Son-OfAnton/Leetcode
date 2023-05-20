af9bc79b0ad59a5bafd08d39726c53a6087e1786
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-20 22:52:45 +0300

    Time: 2900 ms (41.41%), Space: 85 MB (60.82%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,70 @@
+class Union_find:
+    def __init__(self, points):
+        self.rep = {tuple(point): tuple(point) for point in points}
+        self.rank = defaultdict(lambda: 1)
+
+    def find(self, x: int) -> int:
+        if x == self.rep[x]:
+            return x
+        else:
+            self.rep[x] = self.find(self.rep[x])
+            return self.rep[x]
+
+    def union(self, x: int, y: int) -> None:
+        x_rep = self.find(x)
+        y_rep = self.find(y)
+
+        x_rep = self.find(x)
+        y_rep = self.find(y)
+
+        if self.rank[x_rep] < self.rank[y_rep]:
+            self.rep[x_rep] = y_rep
+            self.rank[y_rep] += self.rank[x_rep]
+        else:
+            self.rep[y_rep] = x_rep
+            self.rank[x_rep] += self.rank[y_rep]
+
+    def connected(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        edge_info = []
+
+
+        # Kruskal's algorithm to find minimum spanning tree
+        for i in range(n):
+            for j in range(i + 1, n):
+                r1, c1 = points[i]
+                r2, c2 = points[j]
+                dist = abs(r1 - r2) + abs(c1 - c2)
+                edge_info.append([dist, points[i], points[j]])
+
+
+        edge_info.sort()
+        uf = Union_find(points)
+        min_cost = 0
+
+        edge_count = 0
+        for cost, p_1, p_2 in edge_info:
+            p_1, p_2 = tuple(p_1), tuple(p_2)
+            if not uf.connected(p_1, p_2):
+                uf.union(p_1, p_2)
+                min_cost += cost
+                edge_count += 1
+
+            if edge_count == n - 1:
+                break
+
+        return min_cost
+
+
+
+
+
+
+
+
+
+
