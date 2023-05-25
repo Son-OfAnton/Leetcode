76b7424b2eef148a3cdbb3788f9c8d50e293635f
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-25 18:05:47 +0300

    Time: 75 ms (40.22%), Space: 17.3 MB (34.83%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price_till_now = float("inf")
+        max_profit = 0
+        
+        for i, price in enumerate(prices):
+            price_diff = price - min_price_till_now
+
+            if price_diff > 0:
+                max_profit += price_diff
+                min_price_till_now = float("inf")
+
+            min_price_till_now = min(min_price_till_now, price)
+
+        return max_profit
