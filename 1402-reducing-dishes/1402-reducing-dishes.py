790b91ddf604596698e286dc24a763a0751e6cab
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-27 21:25:49 +0300

    Time: 156 ms (18.73%), Space: 16.5 MB (14.21%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -2,20 +2,21 @@ class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         n = len(satisfaction)
         satisfaction.sort(reverse=True)
-        maxx, best_dishes = satisfaction[0], deque([satisfaction[0]])
+        maxx, best_dishes = satisfaction[0], [satisfaction[0]]
 
         for i in range(1, n):
-            best_dishes.appendleft(satisfaction[i])
+            best_dishes.append(satisfaction[i])
             time = 1
             curr = 0
 
-            for dish in best_dishes:
-                curr += dish * time
+            for j in range(len(best_dishes) - 1, -1, -1):
+                curr += best_dishes[j] * time
                 time += 1
+                
             if curr >= maxx:
                 maxx = curr
             else:
-                best_dishes.popleft()
+                best_dishes.pop()
 
         return maxx if maxx > 0 else 0
 
