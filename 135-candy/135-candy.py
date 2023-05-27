7eb2b4e11e6bc50e7600c1537feeca0aded2869c
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-28 00:18:26 +0300

    Time: 161 ms (86.88%), Space: 19.2 MB (45.95%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in range(1, n):
             if ratings[i]  > ratings[i - 1]:
-                candy_num[i] = max(candy_num[i], candy_num[i - 1] + 1)
+                candy_num[i] = candy_num[i - 1] + 1
         
         for i in range(n - 2, -1, -1):
             if ratings[i]  > ratings[i + 1]:
