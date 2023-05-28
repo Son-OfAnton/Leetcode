6e62b3092ae5a09eb86950de09396e6633338dbc
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-28 16:01:41 +0300

    Time: 73 ms (32.01%), Space: 16.4 MB (28.73%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,9 +1,11 @@
 class Solution:        
     def minCostClimbingStairs(self, cost: List[int]) -> int: 
         n = len(cost)
-        dp = [0] * n
         
         for stair in range(n - 3, -1, -1):
             cost[stair] = cost[stair] + min(cost[stair + 1], cost[stair + 2])
             
         return min(cost[0], cost[1])
+    
+# We can use the cost array itself as a dp array. And build
+# our solution form the last to beginning.
