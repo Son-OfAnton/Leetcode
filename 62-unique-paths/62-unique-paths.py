d92119e55feb16abffcf7efc2bffc5d33c4d8064
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-30 00:27:42 +0300

    Time: 49 ms (25.23%), Space: 16.6 MB (6.06%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -3,7 +3,7 @@ class Solution:
         if m == 1 and n == 1:
             return 1
 
-        dp = [[0] * n for _ in range(m)]
+        dp = defaultdict(int)
 
         def helper(rx: int, cx: int) -> int:
             if rx == m - 1 and cx == n - 1:
@@ -11,9 +11,9 @@ class Solution:
             if rx == m - 1 or cx == n - 1:
                 return 1
 
-            if dp[rx][cx] == 0:
-                dp[rx][cx] = helper(rx + 1, cx) + helper(rx, cx + 1)
+            if dp[(rx, cx)] == 0:
+                dp[(rx, cx)] = helper(rx + 1, cx) + helper(rx, cx + 1)
 
-            return dp[rx][cx]
-
+            return dp[(rx, cx)]
+        
         return helper(0, 0)
