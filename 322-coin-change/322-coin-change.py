cc695c83c53e9450d0bc57cb1bf1a01201981844
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-30 22:27:26 +0300

    Time: 1557 ms (31.60%), Space: 47.2 MB (5.19%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = dict()
+
+        def helper(curr_amount):        
+            if curr_amount == 0:
+                return 0
+
+            if curr_amount not in dp:
+                min_depth = float("inf")
+
+                for coin in coins:
+                    remaining = curr_amount - coin
+                    if remaining >= 0:
+                        min_depth = min(min_depth, helper(remaining))
+
+                dp[curr_amount] = min_depth + 1
+            
+            return dp[curr_amount]
+
+        res = helper(amount)
+
+        return res if res != float("inf") else -1
+
+            
