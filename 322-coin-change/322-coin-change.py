e8f7590ae039c8f83fc375150498e0209d4d3a44
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-05-31 21:56:06 +0300

    Time: 1296 ms (78.01%), Space: 16.7 MB (52.61%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,25 +1,53 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = dict()
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+        
+        for curr_amount in range(amount + 1):
+            for coin in coins:
+                if curr_amount >= coin:
+                    dp[curr_amount] = min(dp[curr_amount], dp[curr_amount - coin] + 1)
+                    
+        return dp[-1] if dp[-1] != float("inf") else -1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+"""   
+    dp = dict()
 
-        def helper(curr_amount):        
-            if curr_amount == 0:
-                return 0
+    def helper(curr_amount):        
+        if curr_amount == 0:
+            return 0
 
-            if curr_amount not in dp:
-                min_depth = float("inf")
+        if curr_amount not in dp:
+            min_depth = float("inf")
 
-                for coin in coins:
-                    remaining = curr_amount - coin
-                    if remaining >= 0:
-                        min_depth = min(min_depth, helper(remaining))
+            for coin in coins:
+                remaining = curr_amount - coin
+                if remaining >= 0:
+                    min_depth = min(min_depth, helper(remaining))
 
-                dp[curr_amount] = min_depth + 1
-            
-            return dp[curr_amount]
+            dp[curr_amount] = min_depth + 1
 
-        res = helper(amount)
+        return dp[curr_amount]
 
-        return res if res != float("inf") else -1
+    res = helper(amount)
 
-            
+    return res if res != float("inf") else -1
+
+"""
