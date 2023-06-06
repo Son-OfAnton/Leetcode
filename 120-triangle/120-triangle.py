d14580da4456c27108b9732f57a8ff7f80c86b85
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-06-06 21:52:47 +0300

    Time: 82 ms (42.01%), Space: 17.4 MB (42.87%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = defaultdict(int)
-
+        # dp = defaultdict(int)
+        dp = [0] * (len(triangle) + 1)
         for row in reversed(triangle):
             for i, curr in enumerate(row):
                 dp[i] = curr + min(dp[i], dp[i + 1])
