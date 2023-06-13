78f5ff84b604edc73aa8968068c0319ed608e250
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-06-13 23:27:07 +0300

    Time: 332 ms (61.15%), Space: 18.9 MB (69.01%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -7,13 +7,12 @@ class Solution:
             return 0 <= rx < m and 0 <= cx < n
         
         def dfs(rx, cx):
-            visited.add((rx, cx))
+            grid[rx][cx] = '0'
             
             for dr, dc in dirs:
                 nr, nc = rx + dr, cx + dc
                 
-                if in_bound(nr, nc) and (nr, nc) not in visited \
-                and grid[nr][nc] == '1':
+                if in_bound(nr, nc) and grid[nr][nc] == '1':
                     dfs(nr, nc)
                 
                 
@@ -22,7 +21,7 @@ class Solution:
         
         for rx in range(m):
             for cx in range(n):
-                if grid[rx][cx] == '1' and (rx, cx) not in visited:
+                if grid[rx][cx] == '1':
                     dfs(rx, cx)
                     island_count += 1
                     
