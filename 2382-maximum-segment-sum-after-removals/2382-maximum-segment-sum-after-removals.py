098beed808faf8c1fdb9ded950cdce5d75fe0ff7
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-03 00:48:25 +0300

    Time: 1502 ms (31.81%), Space: 48.2 MB (50.65%) - LeetHub

--- 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.py
+++ 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.py
@@ -13,20 +13,21 @@ class Union_find:
         x_rep = self.find(x)
         y_rep = self.find(y)
 
-        if x_rep != y_rep:
-            self.seg_sum[x_rep] += self.seg_sum[y_rep]
-            self.rep[y_rep] = x_rep
-            self.max_seg_sum = max(self.max_seg_sum, self.seg_sum[x_rep])
+        self.seg_sum[x_rep] += self.seg_sum[y_rep]
+        self.rep[y_rep] = x_rep
+        self.max_seg_sum = max(self.max_seg_sum, self.seg_sum[x_rep])
 
-    def combine_segment(self, x, val):
-        self.rep[x] = x
+    def plug(self, removed_idx, val):
+        self.rep[removed_idx] = removed_idx
         self.max_seg_sum = max(self.max_seg_sum, val)
-        self.seg_sum[x] = val
+        self.seg_sum[removed_idx] = val
 
-        if x - 1 in self.rep:
-            self.union(x - 1, x)
-        if x + 1 in self.rep:
-            self.union(x + 1, x)
+        # unioning segments if they exist at left
+        # or right of newly plugged num
+        if removed_idx - 1 in self.rep:
+            self.union(removed_idx - 1, removed_idx)
+        if removed_idx + 1 in self.rep:
+            self.union(removed_idx + 1, removed_idx)
 
 
 class Solution:
@@ -37,8 +38,16 @@ class Solution:
 
         for i in range(n - 1, -1, -1):
             res[i] = uf.max_seg_sum
-            query = removeQueries[i]
-            uf.combine_segment(query, nums[query])
+            removed_idx = removeQueries[i]
+            uf.plug(removed_idx, nums[removed_idx])
             
         return res
+
+# Note that all numbers will be removed eventually. We start with one big segment 
+# i.e the array. Since we don't have 'disconnect in union we can't remove a single
+# number and split a segment into two. Instead if we start from the last removed
+# number we can build up the segments. So for every iteration we create a single 
+# segment buy plugging a number and its left and right, if we find numbers to its 
+# side we union them and make a bigger segment and update our rolling max_seg_sum
+# and set this max_seg_sum for the next idx of the iteration as a result.
         
