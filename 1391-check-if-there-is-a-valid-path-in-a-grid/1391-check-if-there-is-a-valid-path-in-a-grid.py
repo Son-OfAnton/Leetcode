75babbdd7251a3a640269e0b5d893822edc131c2
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-03 23:58:31 +0300

    Time: 1611 ms (68.69%), Space: 32 MB (69.97%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -4,7 +4,8 @@ class Solution:
         if m * n == 1:
             return True
 
-        allowed = {1: {(0, 1): {1, 3, 5}, (0, -1): {1, 4, 6}},
+        allowed = {
+                    1: {(0, 1): {1, 3, 5}, (0, -1): {1, 4, 6}},
                     2: {(1, 0): {2, 5, 6}, (-1, 0): {2, 3, 4}},
                     3: {(0, -1): {1, 4, 6}, (1, 0): {2, 5, 6}},
                     4: {(0, 1): {1, 3, 5}, (1, 0): {2, 5, 6}},
@@ -23,12 +24,12 @@ class Solution:
             pipe1 = grid[r][c]
 
             for dr, dc in direction:
-                dirr = (dr, dc)
+                way = (dr, dc)
                 nr, nc = r + dr, c + dc
-                if inbound(nr, nc) and (nr, nc) not in seen:
+                if way in allowed[pipe1] and inbound(nr, nc) and (nr, nc) not in seen:
                     pipe2 = grid[nr][nc]
 
-                    if dirr in allowed[pipe1] and pipe2 in allowed[pipe1][dirr]:
+                    if pipe2 in allowed[pipe1][way]:
                         if (nr, nc) == (m - 1, n - 1):
                             return True
                         queue.append((nr, nc))
