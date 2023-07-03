7b1db24427e8e0ae4f356a525a1d9a9a13d56d12
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-03 23:33:19 +0300

    Time: 1729 ms (60.38%), Space: 32.1 MB (69.33%) - LeetHub

---
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,40 @@
+class Solution:
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+        m, n = len(grid), len(grid[0])
+        if m * n == 1:
+            return True
+
+        allowed = {1: {(0, 1): {1, 3, 5}, (0, -1): {1, 4, 6}},
+                    2: {(1, 0): {2, 5, 6}, (-1, 0): {2, 3, 4}},
+                    3: {(0, -1): {1, 4, 6}, (1, 0): {2, 5, 6}},
+                    4: {(0, 1): {1, 3, 5}, (1, 0): {2, 5, 6}},
+                    5: {(0, -1): {1, 4, 6}, (-1, 0): {2, 3, 4}},
+                    6: {(0, 1): {1, 3, 5}, (-1, 0): {2, 4 ,3}}
+                }
+
+        def inbound(r, c):
+            return 0 <= r < m and 0 <= c < n
+
+        direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        queue, seen = deque([(0, 0)]), set([(0, 0)])
+
+        while queue:
+            r, c = queue.popleft()
+            pipe1 = grid[r][c]
+
+            for dr, dc in direction:
+                dirr = (dr, dc)
+                nr, nc = r + dr, c + dc
+                if inbound(nr, nc) and (nr, nc) not in seen:
+                    pipe2 = grid[nr][nc]
+
+                    if dirr in allowed[pipe1] and pipe2 in allowed[pipe1][dirr]:
+                        if (nr, nc) == (m - 1, n - 1):
+                            return True
+                        queue.append((nr, nc))
+                        seen.add((nr, nc))
+
+        return False
+
+    
+        
