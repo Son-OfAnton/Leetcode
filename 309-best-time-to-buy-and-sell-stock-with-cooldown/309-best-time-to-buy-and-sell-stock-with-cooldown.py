0ffa215e63554428ab229c979a8eecd00e650b19
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-12 00:20:01 +0300

    Time: 70 ms (20.92%), Space: 20.1 MB (41.86%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = dict()
+
+        def helper(i, has_stock):
+            if i >= n:
+                return 0
+            if (i, has_stock) in dp:
+                return dp[(i, has_stock)]
+            
+            if not has_stock:
+                buy = helper(i+1, not has_stock) - prices[i]
+                cooldown = helper(i+1, has_stock)
+                dp[(i, has_stock)] = max(buy, cooldown)
+            else:
+                sell = helper(i+2, not has_stock) + prices[i]
+                cooldown = helper(i+1, has_stock)
+                dp[(i, has_stock)] = max(sell, cooldown)
+            
+            return dp[(i, has_stock)]
+        
+        return helper(0, False)
+
+        
