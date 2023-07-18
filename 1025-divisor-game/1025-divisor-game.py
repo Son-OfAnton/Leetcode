da2bde2579cd98966dccdbb788c3a0bd9241f887
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-19 00:44:49 +0300

    Time: 34 ms (98.25%), Space: 16.3 MB (69.11%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        return n % 2 == 0
+    
+    
+# whoever got an even number can always choose 1 as a divisor
+# and pass an odd number to the other player. So the one with
+# an odd number eventualy reach 1 where he/she can't find any divisor.
