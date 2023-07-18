9ae6b0552e44e4213a3102f6de8b263904041e4e
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-19 00:00:48 +0300

    Time: 5124 ms (52.35%), Space: 32.5 MB (19.53%) - LeetHub

--- 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -12,3 +12,33 @@ class Solution:
             nums.add(num)
 
         return total_cost % (10**9 + 7)
+    
+    
+    
+"""
+Fenwick tree aka Binary indexed tree implementation
+m = max(instructions)
+tree = [0] * (m + 1)
+
+def update(x):
+    while x <= m:
+        tree[x] += 1
+        x += x & -x
+
+def get(x):
+    sum_ = 0
+    while x > 0:
+        sum_ += tree[x]
+        x -= x & -x
+
+    return sum_
+
+total_cost = 0
+for i, num in enumerate(instructions):
+    total_cost += min(get(num - 1), i - get(num))
+    update(num)
+
+return total_cost % (10**9 + 7)
+
+
+"""
