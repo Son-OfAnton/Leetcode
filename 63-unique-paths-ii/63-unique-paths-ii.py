d7c7d0926496d0358ace0bf5776d6a500f4520c1
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-19 22:40:56 +0300

    Time: 65 ms (18.39%), Space: 16.3 MB (84.86%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -3,8 +3,6 @@ class Solution:
         if obstacleGrid[0][0] == 1:
             return 0
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        # dp = defaultdict(int)
-        # dp[(m - 1, n - 1)] = 1
         dp = [[0]*n for _ in range(m)]
         dp[m-1][n-1] = 1
         
@@ -12,15 +10,12 @@ class Solution:
             if r >= m or c >= n or obstacleGrid[r][c] == 1:
                 return 0
                         
-            # return dp[(r,c)]  
             return dp[r][c]
         
         for r in range(m - 1, -1, -1):
             for c in range(n - 1, -1, -1):
-                # dp[(r,c)] += path_getter(r + 1, c) + path_getter(r, c + 1)
                 dp[r][c] += path_getter(r + 1, c) + path_getter(r, c + 1)
                 
-        # return dp[(0, 0)]
         return dp[0][0]
         
 """
