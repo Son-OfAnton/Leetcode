374e86da4378970949f13c09ecef1d2fdb1737e3
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-19 22:40:20 +0300

    Time: 53 ms (82.85%), Space: 16.2 MB (98.15%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -3,20 +3,25 @@ class Solution:
         if obstacleGrid[0][0] == 1:
             return 0
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = defaultdict(int)
-        dp[(m - 1, n - 1)] = 1
+        # dp = defaultdict(int)
+        # dp[(m - 1, n - 1)] = 1
+        dp = [[0]*n for _ in range(m)]
+        dp[m-1][n-1] = 1
         
         def path_getter(r, c):
             if r >= m or c >= n or obstacleGrid[r][c] == 1:
                 return 0
-            
-            return dp[(r,c)]    
+                        
+            # return dp[(r,c)]  
+            return dp[r][c]
         
         for r in range(m - 1, -1, -1):
             for c in range(n - 1, -1, -1):
-                dp[(r,c)] += path_getter(r + 1, c) + path_getter(r, c + 1)
+                # dp[(r,c)] += path_getter(r + 1, c) + path_getter(r, c + 1)
+                dp[r][c] += path_getter(r + 1, c) + path_getter(r, c + 1)
                 
-        return dp[(0, 0)]
+        # return dp[(0, 0)]
+        return dp[0][0]
         
 """
 m, n = len(obstacleGrid), len(obstacleGrid[0])
