e86e6350bdb3b7ec738d05e4cdeb7bd5bff51956
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-19 10:57:48 +0300

    Time: 371 ms (5.29%), Space: 16.7 MB (14.31%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = defaultdict(int)
+
+        def path_finder(r: int, c: int) -> int:
+            if r >= m or c >= n:
+                return 0
+            if obstacleGrid[r][c] == 1:
+                return 0
+            if r == m-1 and c == n-1:
+                return 1
+
+            if dp[(r, c)] == 0:
+                dp[(r, c)] = path_finder(r+1, c) + path_finder(r, c+1)
+
+            return dp[(r, c)]
+        
+        return path_finder(0, 0)
