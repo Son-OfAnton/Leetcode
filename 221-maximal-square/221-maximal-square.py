0f0fcc6e5472c1609cc4a0fb2a0066a0624ffb80
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-22 23:23:49 +0300

    Time: 775 ms (32.21%), Space: 19.4 MB (47.87%) - LeetHub

--- 221-maximal-square/221-maximal-square.py
+++ 221-maximal-square/221-maximal-square.py
@@ -1,16 +1,23 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        dp = [[0]*n for _ in range(m)]
+        # dp = [[0]*n for _ in range(m)]
         max_side = 0
         
         for r in range(m):
             for c in range(n):
                 if matrix[r][c] == '1':
-                    dp[r][c] = 1
+                    matrix[r][c] = 1
                     if r > 0 and c > 0:
-                        dp[r][c] += min(dp[r-1][c], dp[r][c-1], dp[r-1][c-1])
+                        matrix[r][c] += min(int(matrix[r-1][c]), int(matrix[r][c-1]), 
+                                            int(matrix[r-1][c-1]))
                     
-                    max_side = max(max_side, dp[r][c])
-                    
+                    max_side = max(max_side, matrix[r][c])
+        
         return max_side ** 2
+    
+    
+# When we find a cell with value '1', we want to find the 
+# dimension of the largest square ending at that cell. 
+# If a cell is '1' then it belongs to the smallest square
+# adjecent to it. The adjaceny will be to its left, top, top=left.
