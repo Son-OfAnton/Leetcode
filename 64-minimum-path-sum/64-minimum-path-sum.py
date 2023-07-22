a385cd9b11ec52c9c2222718c5c6b0ee2ccd69de
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-22 16:35:05 +0300

    Time: 135 ms (14.13%), Space: 18.1 MB (36.95%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,21 +1,23 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        dp = defaultdict(int)
-        dp[(m-1,n-1)] = grid[m-1][n-1]
+        dp = [[0]*n for _ in range(m)]
+        # dp[(m-1,n-1)] = grid[m-1][n-1]
+        dp[m-1][n-1] = grid[m-1][n-1]
         
         def path_getter(r, c):
             if 0 <= r < m and 0 <= c < n:
-                return dp[(r,c)]
+                # return dp[(r,c)]
+                return dp[r][c]
             return float('inf')
         
         for r in range(m-1, -1, -1):
             for c in range(n-1, -1, -1):
                 if r == m-1 and c == n-1:
                     continue
-                dp[(r,c)] = grid[r][c] + min(path_getter(r+1, c), 
+                dp[r][c] = grid[r][c] + min(path_getter(r+1, c), 
                                              path_getter(r, c+1))
                 
-        return dp[(0,0)]
+        return dp[0][0]
             
             
