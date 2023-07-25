91bcbe509e4073948dc630863d104a2d6ae53f39
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-25 14:44:35 +0300

    Time: 61 ms (26.28%), Space: 16.7 MB (19.26%) - LeetHub

---
+++ 139-word-break/139-word-break.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        word_set = set(wordDict)
+        dp = [None] * (n+1)
+        dp[n] = True
+            
+        def word_checker(start):
+            if dp[start] == None:
+                for end in range(start + 1, n + 1):  
+                    word = s[start:end] 
+                    if word in word_set and word_checker(end):
+                        dp[start] = True
+                        return True
+                    
+                dp[start] = False
+                return False
+            
+            return dp[start]
+
+        return word_checker(0)
+    
+    
+    
