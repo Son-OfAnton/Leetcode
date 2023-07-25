e01b12b67bab828d2d92f7e6463fb8b7002ddfa9
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-07-25 10:58:35 +0300

    Time: 51 ms (30.90%), Space: 16.3 MB (57.43%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # Iterative backtracking
         if not digits:
             return []
         
@@ -19,3 +20,28 @@ class Solution:
         backtrack("", digits)
         
         return all_comb
+    
+    
+"""
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        
+        keypad_map = {'2':"abc", '3':"def", '4':"ghi", '5':"jkl", '6':"mno",
+                      '7':"pqrs", '8':"tuv", '9':"wxyz"}
+                
+        all_comb = []
+        
+        def backtrack(curr_comb, digits_slice):
+            if not digits_slice:
+                all_comb.append(curr_comb)
+                return
+            
+            for letter in keypad_map[digits_slice[0]]:
+                backtrack(curr_comb + letter, digits_slice[1:])
+                
+        backtrack("", digits)
+        
+        return all_comb
+"""
