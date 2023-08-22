86f85ada9a15e9c622fa4a5865d0334789b55d35
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-08-23 02:21:59 +0300

    Time: 1060 ms (84.61%), Space: 22.2 MB (56.72%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -13,3 +13,8 @@ class Solution:
                 starting_index = i + 1
 
         return starting_index if net >= 0 else -1
+    
+# Calculate the curr gas change and net amount. If the curr gas drops
+# below zero we will clear our tank and start from the next station.
+# At last if the net gain if below zero that means the route consumes
+# more gas than it provides through its stations.
