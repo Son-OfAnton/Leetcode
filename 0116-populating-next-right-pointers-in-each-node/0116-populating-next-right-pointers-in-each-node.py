d2246d8918432617a2719a9c6cd5f5ca65ab79c1
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-09-12 01:01:32 +0300

    Time: 145 ms (5.21%), Space: 18 MB (65.98%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,48 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        def connectTwoNodes(left_child, right_child):
+            if not left_child or not right_child:
+                return
+
+            left_child.next = right_child
+
+            connectTwoNodes(left_child.left, left_child.right)
+            connectTwoNodes(right_child.left, right_child.right)
+            connectTwoNodes(left_child.right, right_child.left)
+
+        if not root:
+            return None
+
+        connectTwoNodes(root.left, root.right)
+        return root
+
+"""
+if not root: 
+    return None
+
+queue = deque([root])
+while queue:
+    n = len(queue)
+    for i in range(n):
+        curr = queue.popleft()
+        # Since 'next' of every node is None by default
+        # we will leave the last node as it is
+        if i != n - 1:
+            curr.next = queue[0]
+        # No need to check curr.right since it is perfect BT
+        if curr.left:   
+            queue.append(curr.left)
+            queue.append(curr.right)
+
+return root
+"""
