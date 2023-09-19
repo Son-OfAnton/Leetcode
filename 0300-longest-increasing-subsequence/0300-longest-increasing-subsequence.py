0ab7364cbf04239c6c62744bfa9b00a289bbd868
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-09-19 11:57:48 +0300

    Time: 1850 ms (70.35%), Space: 16.7 MB (28.57%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -8,3 +8,9 @@ class Solution:
         
         return max(dp.values()) if dp.values() else 1
 
+    
+# dp[i] denotes the LIS ending at i.The 'dp[i] <= dp[j]' part 
+# in the checking is to ignore repeatednumbers in the subsequence
+# like [1,2,3,3,4]. In the above case only the first 3 is included 
+# in the LIS. Only In case of all same numbers like Ex3 the dp will 
+# empty so we return 1. 
