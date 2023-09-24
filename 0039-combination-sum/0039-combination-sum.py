5250161e7101e38a7264f704844a583d85fa7d10
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-09-25 01:04:52 +0300

    Time: 38 ms (99.82%), Space: 16.3 MB (94.49%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,37 +1,37 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
-        unique_combinations = []
+        dp = [[] for _ in range(target + 1)]
+        dp[0] = [[]]
 
-        def backtrack(start, target, combination):
-            if target == 0:
-                unique_combinations.append(combination[:])
-                return
-            if target < 0:
-                return
+        for cand in candidates:
+            for i in range(cand, target + 1):
+                for combination in dp[i - cand]:
+                    dp[i].append(combination + [cand])
 
-            for i in range(start, len(candidates)):
-                if i > start and candidates[i] == candidates[i - 1]:
-                    continue
-                combination.append(candidates[i])
-                backtrack(i, target - candidates[i], combination)
-                combination.pop()
-
-        backtrack(0, target, [])
-        return unique_combinations
+        return dp[target]
 
         
         
 """ 
-dp = [[] for _ in range(target + 1)]
-dp[0] = [[]]
+candidates.sort()
+unique_combinations = []
 
-for cand in candidates:
-    for i in range(cand, target + 1):
-        for combination in dp[i - cand]:
-            dp[i].append(combination + [cand])
+def backtrack(start, target, combination):
+    if target == 0:
+        unique_combinations.append(combination[:])
+        return
+    if target < 0:
+        return
 
-return dp[target]
+    for i in range(start, len(candidates)):
+        if i > start and candidates[i] == candidates[i - 1]:
+            continue
+        combination.append(candidates[i])
+        backtrack(i, target - candidates[i], combination)
+        combination.pop()
+
+backtrack(0, target, [])
+return unique_combinations
 
 ---------------------------------------------------------------------------
 
