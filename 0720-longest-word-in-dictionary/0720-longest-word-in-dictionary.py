1b7044125ff0fa8a82dda3007503eb4609a0602b
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-09-29 11:35:57 +0300

    Time: 163 ms (22.47%), Space: 17.7 MB (28.27%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -0,0 +1,61 @@
+class TrieNode:
+    
+    def __init__(self):
+        
+        self.is_end = False
+        self.children = dict()
+        
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.is_end = True
+
+
+    def find_end_count(self, word: str) -> bool:
+        curr = self.root
+        end_count = 0
+        for char in word:
+            curr = curr.children[char]
+            if curr.is_end:
+                end_count += 1
+            else:
+                break
+
+        return end_count
+
+        
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        dictionary = Trie()
+        for word in words:
+            dictionary.insert(word)
+
+        word_end_count = dict()
+        longest_word_built_by_others = None
+        max_end_count = 0
+        
+        for word in words:
+            end_count = dictionary.find_end_count(word)
+            if end_count >= max_end_count:
+                max_end_count = end_count
+                longest_word_built_by_others = word
+            word_end_count[word] = end_count
+
+        if max_end_count == 0:
+            return ''
+        for word, end_count in word_end_count.items():
+            if end_count == max_end_count:
+                longest_word_built_by_others = min(longest_word_built_by_others, 
+                                                    word)
+
+        return longest_word_built_by_others
