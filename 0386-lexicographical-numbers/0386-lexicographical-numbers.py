613e6f6535b3912531dec7b85893b6fb9c5d3ff3
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-10-01 23:31:44 +0300

    Time: 260 ms (6.23%), Space: 34.3 MB (5.06%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 10
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, num):
+        curr = self.root
+        num_str = str(num)
+
+        for char in num_str:
+            digit = int(char)
+            if curr.children[digit] is None:
+                curr.children[digit] = TrieNode()
+            curr = curr.children[digit]
+
+        curr.is_end = True
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        num_trie = Trie()
+        for num in range(1, n + 1):
+            num_trie.insert(num)
+
+        def dfs(node, num):
+            if node.is_end:
+                lexically_sorted_nums.append(num)
+
+            for digit in range(10):
+                child = node.children[digit]
+                if child is not None:
+                    dfs(child, num * 10 + digit)
+
+        lexically_sorted_nums = []
+        for num in range(1, 10):
+            node = num_trie.root.children[num]
+            if node is not None:
+                dfs(node, num)
+
+        return lexically_sorted_nums
