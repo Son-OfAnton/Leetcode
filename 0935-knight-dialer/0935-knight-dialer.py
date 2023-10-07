c4f2d3217e90d1bc1c5197c38081bd2fd2c0151c
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-10-07 17:04:54 +0300

    Time: 2807 ms (23.10%), Space: 91 MB (9.27%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,8 +1,41 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        move_map = {0: (6, 4), 1: (6, 8), 2: (7, 9), 3: (4, 8), 4: (0, 3, 9),
-                    5: (), 6: (0, 1, 7), 7: (2, 6), 8: (1, 3), 9: (2, 4)
+        move_map = {-1: (0,1,2,3,4,5,6,7,8,9), 0: (6, 4), 1: (6, 8), 
+                    2: (7, 9), 3: (4, 8), 4: (0, 3, 9), 5: (), 6: (0, 1, 7), 
+                    7: (2, 6), 8: (1, 3), 9: (2, 4)
                 }
+        
+        dp = defaultdict(int)
+        mod = 10**9 + 7
+
+        def helper(i, hop):
+            if hop == n:
+                return 1
+            if (i,hop) in dp:
+                return dp[(i,hop)]
+
+            for key in move_map[i]:
+                dp[(i,hop)] += helper(key, hop+1)
+                
+            return dp[(i,hop)]
+        
+        return helper(-1, 0) % mod
+    
+# I have used a dummy key -1 to try to simulate starting the hop
+# from every node with the second hop. So we end up with n hops 
+# as a base case instead of n-1 because of the extra hop we've made
+# from that dummy key.
+    
+
+'''
+
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        move_map = {0: (6, 4), 1: (6, 8), 2: (7, 9), 3: (4, 8), 
+                    4: (0, 3, 9), 5: (), 6: (0, 1, 7), 7: (2, 6), 
+                    8: (1, 3), 9: (2, 4)
+                }
+        
         mod = 10**9 + 7
         dp = [[0]*10 for _ in range(n)]
         
@@ -16,3 +49,5 @@ class Solution:
         
     
         return sum(dp[n-1]) % mod
+
+'''
