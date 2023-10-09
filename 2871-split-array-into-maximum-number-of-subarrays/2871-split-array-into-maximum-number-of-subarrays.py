e48b060981dc17ee122300dd072c125f6d8b9322
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-10-09 22:59:20 +0300

    Time: 787 ms (99.23%), Space: 26.7 MB (87.77%) - LeetHub

--- 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
+++ 2871-split-array-into-maximum-number-of-subarrays/2871-split-array-into-maximum-number-of-subarrays.py
@@ -5,13 +5,11 @@ class Solution:
         AND = all_one
         subarray = 0
 
-        for R in range(n):
-            new_AND = AND & nums[R]
-            if new_AND == 0:
+        for num in nums:
+            AND &= num
+            if AND == 0:
                 AND = all_one
                 subarray += 1
-            else: 
-                AND = new_AND
 
         subarray = max(1, subarray)
         return subarray
