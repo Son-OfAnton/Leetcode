d11a8fae044f10cd82129a5efb32faab93ff97a3
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-10-15 21:01:11 +0300

    Time: 673 ms (70.65%), Space: 78.4 MB (37.04%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,5 +1,5 @@
-class Node:
-    def __init__(self, key, val, prev = 'None', next = 'None'):
+class ListNode:
+    def __init__(self, key: int = 0, val: int = 0, prev = None, next = None):
         self.key = key
         self.val = val
         self.prev = prev
@@ -7,40 +7,58 @@ class Node:
 
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}
-        self.dummyHead, self.dummyTail = Node(0, 0), Node(0, 0)
-        self.dummyHead.next = self.dummyTail
-        self.dummyTail.prev = self.dummyHead
-        
+        self.capacity = capacity
+        self.curr_capacity = 0
+        self.DH = ListNode()    # Dummy head
+        self.DT = ListNode()    # Dummy tail
+        self.DH.next = self.DT
+        self.DT.prev = self.DH
+        self.table = dict()
+
+
+    def insert_at_front(self, curr: ListNode) -> None:
+        curr.prev = self.DH
+        curr.next = self.DH.next
+        self.DH.next.prev = curr
+        self.DH.next = curr
+
+    
+    def remove(self, curr: ListNode) -> None:
+        curr.prev.next = curr.next
+        curr.next.prev = curr.prev
+
+
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self._remove(self.cache[key])
-            self._insert(self.cache[key])
-            return self.cache[key].val
-        return -1
+        if key not in self.table:
+            return -1
+        
+        curr = self.table[key]
+        self.remove(curr)
+        self.insert_at_front(curr)   
+
+        return curr.val
+        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self._remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self._insert(self.cache[key])
-        if len(self.cache) > self.cap:
-            lru = self.dummyTail.prev
-            self._remove(lru)
-            del self.cache[lru.key]
-        
-    def _insert(self, node):
-        prev, nxt = self.dummyHead, self.dummyHead.next
-        prev.next, nxt.prev = node, node
-        node.prev, node.next = prev, nxt
-    
-    def _remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
+        if key not in self.table:
+            new_node = ListNode(key, value)
+            self.table[key] = new_node
+            self.insert_at_front(new_node)
+            self.curr_capacity += 1
 
+            if self.curr_capacity > self.capacity:
+                last = self.DT.prev
+
+                # Evicting the Least Recently Used        
+                self.table.pop(last.key)
+                self.remove(last)
+        else:
+            curr = self.table[key]
+            self.remove(curr)
+            self.insert_at_front(curr)
+            curr.val = value
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
