8811f709a4d459d3ec92ad55821a851fe407b843
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-10-18 10:26:18 +0300

    Time: 2804 ms (12.00%), Space: 16.6 MB (77.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -3,14 +3,10 @@ class Solution:
         dp = defaultdict(lambda: 1)
         for i in range(1, len(nums)):
             for j in range(i):
-                if nums[i] > nums[j] and dp[i] <= dp[j]:
-                    dp[i] = dp[j] + 1
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
         
         return max(dp.values()) if dp.values() else 1
 
     
-# dp[i] denotes the LIS ending at i.The 'dp[i] <= dp[j]' part 
-# in the checking is to ignore repeatednumbers in the subsequence
-# like [1,2,3,3,4]. In the above case only the first 3 is included 
-# in the LIS. Only In case of all same numbers like Ex3 the dp will 
-# empty so we return 1. 
+# dp[i] denotes the LIS ending at i. 
