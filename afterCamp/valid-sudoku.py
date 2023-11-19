80d654bb1e12f28410cbe6e01c23004f43042668
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-11-19 22:49:55 +0300

    Added solution for Valid Sudoku

---
+++ afterCamp/valid-sudoku.py
@@ -0,0 +1,63 @@
+# Time - O(81) == O(1)
+# Space - (90 + 90 + 90) == O(1)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rx_elements = defaultdict(set)
+        cx_elements = defaultdict(set)
+        sub_grid = defaultdict(set)
+        
+        for rx in range(9):
+            for cx in range(9):
+                cell = board[rx][cx]
+                top_left_coordinate = (rx//3, cx//3)
+                
+                if cell != '.':
+                    if cell not in rx_elements[rx] and cell not in cx_elements[cx] \
+                    and cell not in sub_grid[top_left_coordinate]:
+                        rx_elements[rx].add(cell)
+                        cx_elements[cx].add(cell)
+                        sub_grid[top_left_coordinate].add(cell)
+                    else:
+                        return False
+                    
+        return True
+        
+
+
+"""
+Time - O(81 + 729 + 162) == O(1)
+Space - O(81 + 81 + 9 + 81) == O(1)
+
+class Solution:
+    def is_valid(self, arr):
+        arr = [char for char in arr if char != '.']
+
+        return len(set(arr)) == len(arr)
+
+    
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Check rows
+        for row in board:
+            if not self.is_valid(row):
+                return False
+
+        # Check columns
+        for col in zip(*board):
+            if not self.is_valid(col):
+                return False
+
+        # Check sub-grids
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                sub_grid = []
+                
+                for x in range(i, i + 3):
+                    for y in range(j, j + 3):
+                        sub_grid.append(board[x][y])
+
+                if not self.is_valid(sub_grid):
+                    return False
+
+        return True
+
+    """
