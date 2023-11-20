44768a401e6edfc1fc3f1f239392cea64ee0d4f8
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-11-21 01:21:12 +0300

    Added solution for Spiral Matrix

---
+++ afterCamp/spiral-matrix.py
@@ -0,0 +1,39 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        spiral = []
+        
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return spiral
+        
+        row_begin = 0
+        col_begin = 0
+        row_end = len(matrix) - 1
+        col_end = len(matrix[0]) - 1
+        
+        while row_begin <= row_end and col_begin <= col_end:
+            for i in range(col_begin, col_end + 1):
+                spiral.append(matrix[row_begin][i])
+            
+            row_begin += 1
+            
+            for i in range(row_begin, row_end + 1):
+                spiral.append(matrix[i][col_end])
+            
+            col_end -= 1
+            
+            if row_begin <= row_end:
+                for i in range(col_end, col_begin - 1, -1):
+                    spiral.append(matrix[row_end][i])
+                    
+            row_end -= 1
+            
+            if col_begin <= col_end:
+                for i in range(row_end, row_begin - 1, -1):
+                    spiral.append(matrix[i][col_begin])
+                    
+            col_begin += 1
+            
+            
+        return spiral
+                    
+    
