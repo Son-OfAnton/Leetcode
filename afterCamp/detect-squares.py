d5e603b40b2324aedea3b838fd8700b8b5862ab6
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-11-22 00:17:21 +0300

    Added solution for Detect Squares

--- afterCamp/detect-squares.py
+++ afterCamp/detect-squares.py
@@ -1,28 +1,31 @@
 class DetectSquares:
 
     def __init__(self):
-        self.store = defaultdict(int)
-        self.pts = []
+        self.store = dict()
 
     def add(self, point: List[int]) -> None:
-        self.store[tuple(point)] += 1
-        self.pts.append(point)
+        point = tuple(point)
+
+        if point in self.store:
+            self.store[point] += 1
+        else:
+            self.store[point] = 1
         
 
     def count(self, point: List[int]) -> int:
         res = 0
         TRx, TRy = point    # top-right x and top-right y
 
-        for BLx, BLy in self.pts:
+        for BLx, BLy in self.store:
             # if the two pts don't align diagonally or 
             # they are on same line/on top of each other
             # the second case creates zero area square
             if abs(BLx - TRx) != abs(BLy - TRy) or BLx == TRx or BLy == TRy:
                 continue
-            diagonal_freq = self.store[(BLx, BLy)]
-            top_left_freq = self.store[(BLx, TRy)]
-            bottom_right_freq = self.store[(TRx, BLy)]
-            res += (top_left_freq * bottom_right_freq)
+            diagonal_freq = self.store.get((BLx, BLy), 0)
+            top_left_freq = self.store.get((BLx, TRy), 0)
+            bottom_right_freq = self.store.get((TRx, BLy), 0)
+            res += diagonal_freq * top_left_freq * bottom_right_freq
 
         return res
 
