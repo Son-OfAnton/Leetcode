04c2f212223d2ee5b0d847dd28821eea05b3c622
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-11-24 01:44:44 +0300

    Added solution for Range Sum Query - Immutable

--- afterCamp/range-sum-query-immutable.py
+++ afterCamp/range-sum-query-immutable.py
@@ -1,18 +1,15 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
-        self.size = len(self.nums)
+        self.nums = [0]
+        self.size = len(nums)
         
-        if self.size > 1:
-            for i in range(1, self.size):
-                self.nums[i] += self.nums[i-1]
-                
+        for i in range(self.size):
+            self.nums.append(self.nums[-1] + nums[i])
+
     def sumRange(self, left: int, right: int) -> int:
-        if left != 0:
-            return self.nums[right] - self.nums[left - 1]
-        else:
-            return self.nums[right]
+        return self.nums[right + 1] - self.nums[left]
+
 
 
 # Your NumArray object will be instantiated and called as such:
