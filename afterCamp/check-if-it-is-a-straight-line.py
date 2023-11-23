0680ca91886a01aadedd51198467a9e41ebd54d1
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-11-24 01:52:52 +0300

    Added solution for Check If It Is a Straight Line

--- afterCamp/check-if-it-is-a-straight-line.py
+++ afterCamp/check-if-it-is-a-straight-line.py
@@ -9,10 +9,8 @@ class Solution:
                     x1, y1 = coordinates[i]
                     x2, y2 = coordinates[j]
                     y_diff, x_diff = y2 - y1, x2 - x1
-                    if x_diff == 0:
-                        slopes.add(inf)
-                        continue
-                    slopes.add(y_diff / x_diff)
+
+                    slopes.add(inf if x_diff == 0 else y_diff / x_diff)
                     if len(slopes) > 1:
                         return False
 
