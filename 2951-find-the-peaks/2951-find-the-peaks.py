10c3aa24105f47046ab9227e62dbd0f730d52cd7
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2024-03-25 19:37:34 +0300

    Time: 54 ms (19.05%), Space: 16.5 MB (46.79%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.py
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -1,3 +1,10 @@
 class Solution:
     def findPeaks(self, mountain: List[int]) -> List[int]:
-        return list(filter(lambda i: mountain[i-1] < mountain[i] > mountain[i+1], range(1, len(mountain) - 1)))
+        peaks = []
+        
+        for i in range(1, len(mountain) - 1):
+            if mountain[i-1] < mountain[i] > mountain[i+1]:
+                peaks.append(i)
+                
+        return peaks
+        
