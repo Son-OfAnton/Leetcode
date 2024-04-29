9029aacce906eb3a9d2cc0cfae8405a4f7327aac
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2024-04-29 22:14:58 +0300

    Time: 1636 ms (26.52%), Space: 28.6 MB (6.45%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        node_map = defaultdict(lambda: [None, None])
+        children = set()
+        
+        for parent, child, is_left in descriptions:
+            if is_left == 1:
+                node_map[parent][0] = child
+            else:
+                node_map[parent][1] = child
+            children.add(child)
+        
+        for parent in node_map:
+            if parent not in children:
+                root = TreeNode(parent, TreeNode(node_map[parent][0]), TreeNode(node_map[parent][1]))
+                break
+                
+        def build_tree(curr_node):
+            if not curr_node:
+                return
+            left_child, right_child = node_map[curr_node.val]
+            curr_node.left = TreeNode(left_child) if left_child else None 
+            curr_node.right = TreeNode(right_child) if right_child else None
+            
+            build_tree(curr_node.left)
+            build_tree(curr_node.right)
+            
+            return curr_node
+        
+#         print(node_map, children)
+        
+#         print(root.val, root.left.val, root.right.val)
+            
+        return build_tree(root)
+            
+            
+        
+                
+        
